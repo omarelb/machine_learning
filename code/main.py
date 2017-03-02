51b531269b385a8cd11212d317eb7594ca03b439
omarelb <omarelb@gmail.com>
2017-03-02 01:38:51 +0100

    .

--- code/main.py
+++ code/main.py
@@ -10,7 +10,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+color = sns.color_palette()
 pd.options.mode.chained_assignment = None  # default='warn'
 
 train_df = pd.read_json("../../data/train.json")
-print(train_df.head())
+test_df = pd.read_json("../../data/test.json")
+#print(train_df.head())
+
+
+#int_level = train_df['interest_level'].value_counts()
+#plt.figure(figsize=(8,4))
+#sns.barplot(int_level.index, int_level.values, alpha=0.8, color=color[1])
+#plt.ylabel('Number of Occurrences', fontsize=12)
+#plt.xlabel('Interest level', fontsize=12)
+
+train_df["interest_level_numerical"] = [mapping[x] for x in train_df["interest_level"].values]
+
+plt.figure()
+plt.axes([0,8,0,4])
+sns.pairplot(train_df, x_vars=["bedrooms", "bathrooms", "price"],\
+             y_vars= "interest_level_numerical", kind="reg")
+plt.show()
+
+#print(train_df.head())
